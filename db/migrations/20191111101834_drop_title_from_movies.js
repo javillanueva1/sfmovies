8f8df18e61619da7bd7e869e9f8b034ff26e4c8e
Joseph Villanueva <joseph@lob.com>
2019-11-15 12:50:00 -0800

    feat(rename_title): Change db migration to use async/await and template literals

--- db/migrations/20191111101834_drop_title_from_movies.js
+++ db/migrations/20191111101834_drop_title_from_movies.js
@@ -1,22 +1,21 @@
 'use strict';
 
-exports.up = (knex) => {
-  return knex.schema.table('movies', (table) => {
+const movies = 'movies';
+const name_constraint = 'movies_name_not_null';
+
+exports.up = async (knex) => {
+  await knex.schema.table('movies', (table) => {
     table.dropColumn('title');
-  })
-  .then(() => {
-    return knex.raw('ALTER TABLE movies ADD CONSTRAINT movies_name_not_null CHECK (name IS NOT NULL) NOT VALID')
-    .then(() => {
-      return knex.raw('ALTER TABLE movies VALIDATE CONSTRAINT movies_name_not_null');
-    });
   });
+
+  await knex.raw(`ALTER TABLE ${movies} ADD CONSTRAINT ${name_constraint} CHECK (name IS NOT NULL) NOT VALID`);
+  await knex.raw(`ALTER TABLE ${movies} VALIDATE CONSTRAINT ${name_constraint}`);
 };
 
-exports.down = (knex) => {
-  return knex.schema.table('movies', (table) => {
+exports.down = async (knex) => {
+  await knex.schema.table('movies', (table) => {
     table.text('title');
-  })
-  .then(() => {
-    return knex.raw('ALTER TABLE movies DROP CONSTRAINT movies_name_not_null');
   });
+
+  await knex.raw(`ALTER TABLE ${movies} DROP CONSTRAINT ${name_constraint}`);
 };
