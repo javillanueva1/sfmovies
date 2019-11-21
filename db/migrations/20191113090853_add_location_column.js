1d6a3f2bf75cf0a2f5234c6fe1526865d433f809
Joseph Villanueva <joseph@lob.com>
2019-11-21 15:03:33 -0800

    feat(multiple_locations): Move from column to table schema design

--- db/migrations/20191113090853_add_location_column.js
+++ db/migrations/20191113090853_add_location_column.js
@@ -1,11 +1,20 @@
 'use strict';
 
 exports.up = async (knex) => {
-  await knex.raw('ALTER TABLE movies ADD COLUMN locations text[]');
+  await knex.schema.createTable('locations', (table) => {
+    table.increments('location_id').primary();
+    table.text('location');
+  });
+
+  await knex.schema.createTable('location_movies', (table) => {
+    table.integer('movie_id');
+    table.integer('location_id');
+    table.foreign('movie_id').references('movies.id');
+    table.foreign('location_id').references('locations.location_id');
+  });
 };
 
 exports.down = async (knex) => {
-  await knex.schema.table('movies', (table) => {
-    table.dropColumn('locations');
-  });
+  await knex.schema.dropTable('location_movies');
+  await knex.schema.dropTable('location');
 };
